ca07771303bd4080ca58391db7acda6060859286
note9999 <sanghoon273@gmail.com>
2023-12-10 01:11:32 +0900

    게시판 쿼리 생성 후 junit test 진행

--- MiniProject/src/main/java/minipj/sh/mapper/BoardMapper.java
+++ MiniProject/src/main/java/minipj/sh/mapper/BoardMapper.java
@@ -1,8 +1,13 @@
 package minipj.sh.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import minipj.sh.entity.Board;
 
 @Mapper
 public interface BoardMapper { // @,XML
 
+	public List<Board> getList();
 }
