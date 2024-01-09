075c8e75554bfb2fe84c08aea3ae17b17d48af3f
note9999 <sanghoon273@gmail.com>
2024-01-09 23:09:39 +0900

    BoardService junit test 완료

---
+++ MiniProject/src/test/java/minipj/sh/service/BoardServiceTest.java
@@ -0,0 +1,26 @@
+package minipj.sh.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import lombok.extern.log4j.Log4j;
+import minipj.sh.mapper.BoardMapperTest;
+
+@Log4j
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+
+public class BoardServiceTest {
+	
+	@Autowired
+	BoardService boardService;
+	
+	@Test
+	public void testGetList() {
+		// List<Board> 
+		boardService.getList().forEach(vo->log.info(vo));
+	}
+}
