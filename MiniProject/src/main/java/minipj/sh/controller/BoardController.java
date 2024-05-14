92734aebf368a86f7634c082b00e4d2430f4adec
note9999 <sanghoon273@gmail.com>
2024-05-14 20:54:15 +0900

    BoardController 구현후  BoardController Test코드 작성

--- MiniProject/src/main/java/minipj/sh/controller/BoardController.java
+++ MiniProject/src/main/java/minipj/sh/controller/BoardController.java
@@ -1,8 +1,29 @@
 package minipj.sh.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import minipj.sh.entity.Board;
+import minipj.sh.service.BoardService;
 
 @Controller  //POJO
+@RequestMapping("/board/*")
 public class BoardController {
-
-}
+	
+	@Autowired
+	BoardService boardService;
+	
+	@GetMapping("/list")
+	public String getList(Model model) {
+		List<Board>list=boardService.getList();
+		//객체바인딩
+		model.addAttribute("list",list);
+		return "board/boardList";
+		
+	}
+} 
